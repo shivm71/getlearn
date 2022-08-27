5cc5488e0a4bd1faaa08db63991c361798b818a4
Shivam <shivamshah71@gmail.com>
2022-08-28 01:37:05 +0530

    created a new ython file and made changes in readme file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # getlearn
 Teaching git to students
+
+
+
---
+++ first.py
@@ -0,0 +1,3 @@
+green color file - new file
+red color file show you - git is ignoring it
+blue color file - this file alreasy exist and you made a change in it
